ab0e76f60676d253e225c0fc4212da0d64f84af8
C M Santosh Kumar <pagadala.kalyani@gmail.com>
2023-03-22 21:06:52 +0000

    models and schemas created

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            required: "Username is required",
+        },
+
+        email: {
+            type: String,
+            unique: true,
+            required: "Username is required",
+            // match: [/.+@.+\..+/],
+            match: /.+\@.+\..+/,
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
